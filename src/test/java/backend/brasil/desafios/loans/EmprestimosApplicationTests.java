77eff142eba5a715a617b9de7f50f0d30b837810
Icarus <icarolmol@gmail.com>
2023-10-19 21:23:31 -0300

    initial commit

---
+++ src/test/java/backend/brasil/desafios/loans/EmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.brasil.desafios.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
