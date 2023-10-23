fac1e500fb1dab6e856ad2bf80f94f90ceaef866
Icarus <icarolmol@gmail.com>
2023-10-23 17:57:59 -0300

    Refactor: implemented exception in post method.

--- src/main/java/backend/brasil/desafios/loans/controller/LoanController.java
+++ src/main/java/backend/brasil/desafios/loans/controller/LoanController.java
@@ -2,8 +2,8 @@ package backend.brasil.desafios.loans.controller;
 
 import backend.brasil.desafios.loans.model.RequestLoan;
 import backend.brasil.desafios.loans.service.LoanOptionsService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.*;
 public class LoanController {
 
     @PostMapping
-    public ResponseEntity loanOptions(@RequestBody @Valid RequestLoan data){
-        return ResponseEntity.ok().body(LoanOptionsService.getOptions(data));
+    public ResponseEntity loanOptions(@RequestBody @Validated RequestLoan data){
+        try {
+            return ResponseEntity.ok().body(LoanOptionsService.getOptions(data));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e);
+        }
     }
 
 
